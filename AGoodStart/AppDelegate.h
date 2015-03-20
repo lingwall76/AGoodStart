98f1cad1a6599a42a889df82fb8cdea69b96856b
lingwall76 <lingwall76@gmail.com>
2015-03-20 14:55:40 -0600

    Adding XCode project
    Adding XCode project

---
+++ AGoodStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGoodStart
+//
+//  Created by Sarah on 3/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AGoodStart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGoodStart
+//
+//  Created by Sarah on 3/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
